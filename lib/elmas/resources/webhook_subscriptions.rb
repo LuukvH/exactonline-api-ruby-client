385c1f6d2944d41899257d91f6196e0004253798
LuukvH <laj@home.nl>
2018-07-02 10:57:38 +0200

    Start of webhook extension

---
+++ lib/elmas/resources/webhook_subscriptions.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Elmas
+  class WebhookSubscriptions
+    # A sales entry needs multiple sales entry lines
+    # It should also have a journal id and a contact id who ordered it
+    include Elmas::Resource
+
+    def base_path
+      "webhooks/WebhookSubscriptions"
+    end
+
+    def mandatory_attributes
+      %i[callback_url topic]
+    end
+
+  end
+end
